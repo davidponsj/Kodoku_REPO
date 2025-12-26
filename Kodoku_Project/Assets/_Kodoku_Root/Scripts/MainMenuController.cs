d15b78a3a1046fff05c6761a2d89a5c5df9bdc07
davidponsj <davidponsj@gmail.com>
2025-12-27 00:36:33 +0100

    Main Menu Imputs
    Input de teclado y mando compatible con el main menu.

--- Kodoku_Project/Assets/_Kodoku_Root/Scripts/MainMenuController.cs
+++ Kodoku_Project/Assets/_Kodoku_Root/Scripts/MainMenuController.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] ButtonController[] botones;
     [SerializeField] string playSceneName;
+    [SerializeField] float navegationCooldown = 0.25f;
 
     ButtonController currentSelected = null;
-    bool isLocked = false;
+    int currentIndex = 0;
 
+    bool isLocked = false;
+    bool canNavegate = true;
+
+    private void Start()
+    {
+        if (botones.Length >  0)
+        {
+            currentSelected = botones[currentIndex];
+            currentSelected.Select();
+        }
+    }
+
+//! RATON
     public void SelectedButton(ButtonController boton)
     {
         if (isLocked) return;
@@ -18,8 +34,10 @@ public class MainMenuController : MonoBehaviour
             currentSelected.Deselect();
         }
 
-        boton.Select();
         currentSelected = boton;
+        currentIndex = System.Array.IndexOf(botones, boton);
+
+        boton.Select();
     }
 
     public void PressedButton(ButtonController boton)
@@ -32,6 +50,49 @@ public class MainMenuController : MonoBehaviour
         }
     }
 
+//! MANDO/TECLADO
+
+    public void OnNavegate(InputAction.CallbackContext context)
+    {
+        if (isLocked || !canNavegate || !context.performed) return;
+
+        float y = context.ReadValue<Vector2>().y;
+
+        if (Mathf.Abs(y) < 0.5f) return;
+
+        int direction = y > 0 ? -1 : 1;
+        ChangeSelection(direction);
+
+    }
+
+    public void OnSubmit (InputAction.CallbackContext context)
+    {
+        if (!context.performed || isLocked) return;
+
+        if (currentSelected != null) StartCoroutine(ButtonAnimation(currentSelected));
+    }
+
+    private void ChangeSelection(int direction)
+    {
+        currentSelected.Deselect();
+
+        currentIndex += direction;
+
+        if (currentIndex < 0) currentIndex = botones.Length - 1;
+        else if (currentIndex >= botones.Length) currentIndex = 0;
+
+        currentSelected = botones[currentIndex];
+        currentSelected.Select();
+
+        StartCoroutine(NavegationCooldown());
+    }
+
+    IEnumerator NavegationCooldown()
+    {
+        canNavegate = false;
+        yield return new WaitForSeconds(navegationCooldown);
+        canNavegate = true;
+    }
     IEnumerator ButtonAnimation(ButtonController boton)
     {
         isLocked = true;
